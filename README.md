f16e1cd5a72de0ebf5de336a73ac16e4e09a86ac
Thafila <thafilakf@gmail.com>
2019-05-28 19:34:06 -0300

    Resolucao no README

---
+++ README.md
@@ -0,0 +1,5 @@
+# Problema da Mochila
+Utilizar o algoritmo de força bruta e identificar qual o tamanho máximo da mochila que o algoritmo é capaz de resolver e calcular o tempo em segundos (minutos ou horas se necessário).
+
+## Resolução
+O tamanho maximo da mochila suportado é 32 apresentando um erro e parando de exucutar.
--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ class Main {
 	  // No 32 ele para de funcionar
 	  // e mostra o número do tamanho máximo da mochila
 	  for(int i=1; i<= 32 ; i++){
-		  Knapsack knapsack = new Knapsack(i);
+		  Knapsack knapsack = new Knapsack(32);
 		  BruteForceAlgorithm brute = new BruteForceAlgorithm();
 		  brute.execute(knapsack);
 		  System.out.println("Numero do tamanho maximo da mochila: "+ i);
